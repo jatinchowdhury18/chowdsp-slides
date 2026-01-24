be9aa3b2a49808b6a158fb17e1c0bd467868bc17
jatin <jatinchowdhury18@gmail.com>
2026-01-23 22:57:48 -0800

    Initial commit

---
+++ slides.h
@@ -0,0 +1,155 @@
+#pragma once
+
+#include <cassert>
+#include <visage/app.h>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+struct MMap_Image
+{
+    const unsigned char* data {};
+    size_t size {};
+
+    MMap_Image() = default;
+
+    MMap_Image (std::string path)
+    {
+        int fd = open (path.c_str(), O_RDONLY);
+        if (fd == -1)
+            assert (false);
+
+        struct stat sb;
+        if (fstat (fd, &sb) == -1)
+            assert (false);
+
+        size = sb.st_size;
+
+        data = static_cast<const unsigned char*> (mmap (NULL, size, PROT_READ, MAP_PRIVATE, fd, 0u));
+        if (data == MAP_FAILED)
+            assert (false);
+
+        close (fd);
+    }
+
+    ~MMap_Image()
+    {
+        if (data != nullptr)
+            munmap (const_cast<unsigned char*> (data), size);
+    }
+};
+
+struct Slide_Params
+{
+    MMap_Image* background_image {};
+    visage::Color background_color {};
+};
+
+struct Slide : visage::Frame
+{
+    Slide_Params params {};
+
+    Slide (Slide_Params slide_params) : params { slide_params }
+    {
+    }
+
+    ~Slide()
+    {
+        if (params.background_image != nullptr)
+            delete params.background_image;
+    }
+
+    void resized() override
+    {
+        redrawAll();
+    }
+
+    void draw (visage::Canvas& canvas) override
+    {
+        canvas.setColor (params.background_color);
+        canvas.fill (0, 0, width(), height());
+
+        if (params.background_image != nullptr)
+        {
+            canvas.setColor (0xffffffff);
+            canvas.image (params.background_image->data, params.background_image->size, 0, 0, width(), height());
+        }
+    }
+
+    bool keyPress (const visage::KeyEvent& key) override
+    {
+        return false;
+    }
+};
+
+struct Slideshow : visage::Frame
+{
+    std::vector<Slide*> slides {};
+    size_t active_slide = 0;
+
+    explicit Slideshow (std::initializer_list<Slide*> init_slides)
+        : slides { init_slides.begin(), init_slides.end() }
+    {
+        for (auto* slide : slides)
+        {
+            addChild (slide, false);
+        }
+        if (! slides.empty())
+        {
+            active_slide = 0;
+            slides[active_slide]->setVisible (true);
+        }
+
+        // requestKeyboardFocus();
+        setAcceptsKeystrokes(true);
+    }
+
+    ~Slideshow() override
+    {
+        for (auto* slide : slides)
+            delete slide;
+    }
+
+    void resized() override
+    {
+        using namespace visage::dimension;
+        for (auto* slide : slides)
+            slide->layout().setDimensions (100_vw, 100_vh);
+    }
+
+    void previous_slide()
+    {
+        if (active_slide == 0)
+            return;
+        slides[active_slide]->setVisible (false);
+        active_slide--;
+        slides[active_slide]->setVisible (true);
+    }
+
+    void next_slide()
+    {
+        if (active_slide == slides.size() - 1)
+            return;
+        slides[active_slide]->setVisible (false);
+        active_slide++;
+        slides[active_slide]->setVisible (true);
+    }
+
+    bool keyPress (const visage::KeyEvent& key) override
+    {
+        if (key.keyCode() == visage::KeyCode::Right)
+        {
+            next_slide();
+            return true;
+        }
+        if (key.keyCode() == visage::KeyCode::Left)
+        {
+            previous_slide();
+            return true;
+        }
+
+        return false;
+    }
+};
