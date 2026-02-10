5349a72aee845aa3e701a4ac143928d31bcc173b
jatin <jatinchowdhury18@gmail.com>
2026-02-09 20:42:00 -0800

    CMake adjustments for external projects

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -1,31 +1,35 @@
-function(package_slides_app)
+function(slides_app target target_dir)
+    add_executable(${target} ${chowdsp_slides_dir}/src/slides_main.cpp)
+    target_link_libraries(${target} PRIVATE visage)
+    target_include_directories(${target} PRIVATE ${target_dir} ${chowdsp_slides_dir}/src)
+
     if (WIN32)
-        set_target_properties(chowdsp_slides PROPERTIES WIN32_EXECUTABLE YES)
-        target_compile_definitions(chowdsp_slides
+        set_target_properties(${target} PROPERTIES WIN32_EXECUTABLE YES)
+        target_compile_definitions(${target}
             PUBLIC
                 NOMINMAX=1
                 _USE_MATH_DEFINES=1
         )
     elseif (APPLE)
-        set(PROGRAM_NAME chowdsp_slides)
-        configure_file(cmake/Info.plist.in ${CMAKE_CURRENT_BINARY_DIR}/chowdsp-slides.plist)
-        set_target_properties(chowdsp_slides PROPERTIES
+        set(PROGRAM_NAME ${target})
+        configure_file(${chowdsp_slides_dir}/cmake/Info.plist.in ${CMAKE_CURRENT_BINARY_DIR}/chowdsp-slides.plist)
+        set_target_properties(${target} PROPERTIES
             MACOSX_BUNDLE TRUE
             MACOSX_BUNDLE_INFO_PLIST ${CMAKE_CURRENT_BINARY_DIR}/chowdsp-slides.plist
         )
     elseif (EMSCRIPTEN)
-        file(GLOB asset_files ${CMAKE_CURRENT_SOURCE_DIR}/slides/assets/*)
+        file(GLOB asset_files ${target_dir}/assets/*)
         set(preload_commands "")
         foreach(asset_file ${asset_files})
-            string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/slides/" "" asset_file_stem ${asset_file})
+            string(REPLACE "${target_dir}/" "" asset_file_stem ${asset_file})
             message(STATUS "Packaging asset file: ${asset_file_stem}")
         set(preload_cmd "SHELL:--preload-file \"${asset_file}@${asset_file_stem}\"")
             list(APPEND preload_commands "${preload_cmd}")
         endforeach()
 
-        target_link_options(chowdsp_slides
+        target_link_options(${target}
             PRIVATE
-            --shell-file ${CMAKE_CURRENT_SOURCE_DIR}/slides/slides.html
+            --shell-file ${target_dir}/slides.html
             ${preload_commands}
             -sGL_ENABLE_GET_PROC_ADDRESS
             -sALLOW_MEMORY_GROWTH
@@ -34,10 +38,10 @@ function(package_slides_app)
             "-sEXPORTED_RUNTIME_METHODS=['ccall', 'cwrap', 'UTF8ToString']"
         )
 
-        set_target_properties(chowdsp_slides PROPERTIES
+        set_target_properties(${target} PROPERTIES
             SUFFIX ".html"
             OUTPUT_NAME "index"
-            RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/slides/web"
+            RUNTIME_OUTPUT_DIRECTORY "${target_dir}/web"
         )
     endif ()
-endfunction(package_slides_app)
+endfunction()
